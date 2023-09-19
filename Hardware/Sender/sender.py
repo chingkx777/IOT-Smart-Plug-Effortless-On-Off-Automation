9fc95583937a1d8a0e6fc803517ba0f84290d081
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-09-20 00:28:25 +0800

    Update sender.py

--- Hardware/Sender/sender.py
+++ Hardware/Sender/sender.py
@@ -2,41 +2,47 @@ import network
 import socket
 import time
 
-# Define your Wi-Fi credentials
-ssid = "HUAWEI nova 7"
-password = "nova7140720"
-
-# Define the IP address and port of the receiver ESP32
-receiver_ip = "192.168.43.120"
-receiver_port = 8080  # Choose a port number (e.g., 8080)
-
-# Connect to Wi-Fi
+# Global Object for Wi-Fi
 wifi = network.WLAN(network.STA_IF)
-wifi.active(True)
-wifi.connect(ssid, password)
 
-# Wait for Wi-Fi connection
-while not wifi.isconnected():
-    pass
+# Global Object for UDP
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("Connected to Wi-Fi")
+class SyncS:
+    def __init__(self, receiver_ip, receiver_port):
+        # Define SSID and Password
+        self.ssid = "HUAWEI nova 7"
+        self.password = "nova7140720"
 
-# Create a socket for communication
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Define the IP address and port of the receiver ESP32
+        self.receiver_ip = receiver_ip
+        self.receiver_port = receiver_port  # Choose a port number 
 
-try:
-    # Connect to the receiver
-    s.connect((receiver_ip, receiver_port))
+        # Connect to Wi-Fi
+        wifi.active(True)
+        wifi.connect(self.ssid, self.password)
+
+        # Wait for Wi-Fi connection
+        while not wifi.isconnected():
+            pass
+
+        print("Connected to Wi-Fi")
+
     
-    # Send a message
-    message = "Hello, ESP32 receiver!"
-    s.send(message.encode('utf-8'))
-    print("Message sent successfully")
-    
-    # Close the socket
-    s.close()
-except Exception as e:
-    print("Error:", e)
+    def text(self, message):
+        # Connect to the receiver
+        udp_socket.connect((self.receiver_ip, self.receiver_port))
+            
+        # Send a message
+        udp_socket.send(message.encode('utf-8'))
+        print("Message sent successfully")
+            
+        # Close the socket
+        udp_socket.close()
+        
+        # Disconnect from Wi-Fi
+        wifi.disconnect()
 
-# Disconnect from Wi-Fi
-wifi.disconnect()
+# Creating Object from Class, and then run method
+# ESP1 = SyncS('192.168.43.16', 8080)
+# ESP1.text('Hi, ESP')

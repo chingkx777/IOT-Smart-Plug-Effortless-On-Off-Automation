62060a17fa42e542325b0394226ad615c8a400bd
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-09-20 13:46:09 +0800

    Update receiver.py

--- Hardware/Receiver/receiver.py
+++ Hardware/Receiver/receiver.py
@@ -2,49 +2,56 @@ import network
 import socket
 import time
 
+# Global Wi-Fi Variable
+wifi = network.WLAN(network.STA_IF)
+
 # Define your Wi-Fi credentials
 ssid = "HUAWEI nova 7"
 password = "nova7140720"
 
-# Define the port on which the receiver should listen
-receiver_port = 8080  # Use the same port number as in the sender
+class SyncR:
+    def __init__(self, receiver_port):
+        # Define the port on which the receiver should listen
+        self.receiver_port = receiver_port
 
-# Create a socket server to receive messages
-def create_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', receiver_port))
-    server.listen(1)
-    print("Server is listening on port", receiver_port)
-    return server
+        # Connect to Wi-Fi
+        wifi.active(True)
+        wifi.connect(ssid, password)
 
-# Connect to Wi-Fi
-wifi = network.WLAN(network.STA_IF)
-wifi.active(True)
-wifi.connect(ssid, password)
-print(wifi.ifconfig())
+        # Wait for Wi-Fi connection
+        while not wifi.isconnected():
+            pass
 
-# Wait for Wi-Fi connection
-while not wifi.isconnected():
-    pass
-
-print("Connected to Wi-Fi")
-
-# Create the server
-server = create_server()
-
-# Main loop to handle incoming connections
-while True:
-    try:
-        conn, addr = server.accept()
-        print("Connected by", addr)
+        print("\nConnected to Wi-Fi")
+        print(f'IP: {wifi.ifconfig()}')
         
+        # Create a socket server to receive messages
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind(('0.0.0.0', self.receiver_port))
+        self.server.listen(1)
+        print("Server is listening on port", self.receiver_port)
+
+        
+        
+    def text(self):
+        # Handle incoming connections
+        conn, addr = self.server.accept()
+        print("Connected by", addr)
+                
         # Receive and print the message
         message = conn.recv(1024).decode('utf-8')
         print("Received message:", message)
-        
-        # Handle the received message as needed
-        
-        # Close the connection
+                
+        # Close the connection, server & Wi-Fi
         conn.close()
-    except Exception as e:
-        print("Error:", e)
+        self.server.close()
+        time.sleep(1)
+        wifi.disconnect()
+        
+        # Return message receive
+        return message
+
+# Creating Object from Class, and then run method
+# t = SyncR(8080)
+# t.text()

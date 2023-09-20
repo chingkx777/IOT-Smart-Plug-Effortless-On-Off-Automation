d2b92dc88d0d24240b292380d39ada19b14726a1
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-09-20 16:42:11 +0800

    Update receiver.py

--- Hardware/Receiver/receiver.py
+++ Hardware/Receiver/receiver.py
@@ -6,8 +6,8 @@ import time
 wifi = network.WLAN(network.STA_IF)
 
 # Define your Wi-Fi credentials
-ssid = "HUAWEI nova 7"
-password = "nova7140720"
+ssid = 'SSID'
+password = 'PASSWORD'
 
 class SyncR:
     def __init__(self, receiver_port):
@@ -26,32 +26,19 @@ class SyncR:
         print(f'IP: {wifi.ifconfig()}')
         
         # Create a socket server to receive messages
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server.bind(('0.0.0.0', self.receiver_port))
-        self.server.listen(1)
         print("Server is listening on port", self.receiver_port)
 
         
         
     def text(self):
-        # Handle incoming connections
-        conn, addr = self.server.accept()
-        print("Connected by", addr)
-                
         # Receive and print the message
-        message = conn.recv(1024).decode('utf-8')
+        data, addr = self.server.recvfrom(1024)
+        message = data.decode('utf-8')
         print("Received message:", message)
-                
-        # Close the connection, server & Wi-Fi
-        conn.close()
-        self.server.close()
-        time.sleep(1)
-        wifi.disconnect()
-        
-        # Return message receive
-        return message
 
 # Creating Object from Class, and then run method
-# t = SyncR(8080)
-# t.text()
+t = SyncR(8080)
+while True:
+    t.text()

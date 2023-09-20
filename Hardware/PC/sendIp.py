ffda8aa9b38b3c379637f432a2959089bad2e746
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-09-20 16:43:29 +0800

    Update sendIp.py

--- Hardware/PC/sendIp.py
+++ Hardware/PC/sendIp.py
@@ -1,18 +1,21 @@
 import socket
+import time
 
 # Define the ESP32's IP address and port
-esp32_ip = "192.168.43.215"  # Replace with the ESP32's IP address
+esp32_ip = "192.168.1.22"  # Replace with the ESP32's IP address
 esp32_port = 8080  # Choose a port number (e.g., 8080)
 
 # Create a socket for communication
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+time.sleep(2)
 
 try:
     # Connect to the ESP32
     s.connect((esp32_ip, esp32_port))
     
     # Send a message
-    message = "Hello, ESP32!"
+    message = "turn on the light"
     s.send(message.encode('utf-8'))
     print("Message sent successfully")
     

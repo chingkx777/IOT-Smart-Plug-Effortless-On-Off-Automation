f49d8078c52b77da076876dec5798673d9f9c9fe
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-09-24 19:23:02 +0800

    Update sr.py

--- Hardware/PC/sr.py
+++ Hardware/PC/sr.py
@@ -1,15 +1,19 @@
+# When using this, make sure your SpeechRecognition & Pyaudio package being installed
 import speech_recognition as sr
 
 class speech2text():
     def __init__(self):
+        # Create an object from class
         self.recognized_text = None
         r = sr.Recognizer()
 
+        # Open your source mic and record
         with sr.Microphone() as source:
             print('\n********************************')
             print('Start recording......\n')
             audio = r.listen(source, timeout=4, phrase_time_limit=3)
 
+            # Speech to Text using Google API
             try:
                 text = r.recognize_google(audio)
                 print(f"Message: {text}\n")

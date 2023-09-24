6c81a2322a5fc1502c057a5c0f008ca65911bb5e
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-09-24 18:05:29 +0800

    Update main.py

--- Hardware/Receiver/main.py
+++ Hardware/Receiver/main.py
@@ -2,18 +2,24 @@ from data_log import Sync
 from checker import ckr
 import time
 
-appliances = ['light', 'fan', 'aircon']
-pin = [23, 22, 21]
+# List all input and output name and gpio
+appliances = ['light', 'fan', 'aircon', 'music']
+pin = [23, 22, 21, 19]
+
+ssid_pc = '192.168.1.6'
+port = 8080
 
 x = ckr(appliances, pin)
 x.my_dict()
-y = Sync(8080)
+y = Sync(ssid_pc, port)
+
 
 while True:
     try:
+        y.status(x.pin())
         command = y.text()
         x.msg(command)
     except KeyboardInterrupt:
-        print('I am going under maintainence......')
-        y.close()
+        print('I am under maintainance')
+        y.close
         break

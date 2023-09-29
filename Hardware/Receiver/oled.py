2f671ef490744944982e1e226f2e7cda8abb4040
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-09-29 15:04:48 +0800

    Update oled.py

--- Hardware/Receiver/oled.py
+++ Hardware/Receiver/oled.py
@@ -3,12 +3,9 @@ from time import sleep
 import ssd1306
 
 class oled:
-    def __init__(self, text, line):
-        self.text = text
-        self.line = line
-        
+    def __init__(self, input_lists):
         # Initialize OLED Pin
-        i2c = SoftI2C(scl=Pin(5), sda=Pin(18))
+        i2c = SoftI2C(scl=Pin(4), sda=Pin(15))
 
         # OLED Width and Height
         oled_width = 128
@@ -16,11 +13,14 @@ class oled:
 
         # Create object from class ssd1306
         oled_obj = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-        
+
         # In specific location, create word
         oled_obj.text('IOT Smart Plug', 0, 0)
-        oled_obj.text(f'IP: 192.168.1.19', 0, 20)
-        oled_obj.text(self.text, 0, self.line)
         
-                
+        # Iterate through the list of input_lists and display text at specified lines
+        for input_list in input_lists:
+            if len(input_list) == 2:
+                text, line = input_list
+                oled_obj.text(text, 0, line)
+        
         oled_obj.show()

941286ccaaefd5526c32c58ea6480fce2a2cb790
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-09-29 13:23:35 +0800

    Update button.py

--- Hardware/Receiver/button.py
+++ Hardware/Receiver/button.py
@@ -1,28 +1,24 @@
-from machine import Pin
 import time
+from machine import Pin
 
-class button_On_Off:
+class ButtonOnOff:
     def __init__(self, button_pin, output_pin):
         self.button_pin = button_pin
-        self.output_pin = output_pin    
+        self.output_pin = output_pin
+        self.button_states = [0] * len(button_pin)  # Initialize button states to 0 (not pressed)
 
-        button = Pin(self.button_pin, Pin.IN, Pin.PULL_UP)
-        output = Pin(self.output_pin, Pin.OUT)
+        # Configure button pins as input with pull-up resistor
+        self.buttons = [Pin(b, Pin.IN, Pin.PULL_UP) for b in button_pin]
+        self.outputs = [Pin(o, Pin.OUT) for o in output_pin]
 
-        while True:
-            # Variable to track the button state and output state 
-            button_state = button.value()
-            output_state = output.value()
-            
-            # Process
-            if button_state == 0: # Button Pressed
-                if output_state == 0:
-                    output.on()
-                    time.sleep_ms(200)
-                else:
-                    output.off()
-                    time.sleep_ms(200)
-                    
-# Creating Object from Class
-# button_On_Off(input_pin, output_pin)
-button_On_Off(25, 2)
+    def check_buttons(self):
+        for i, button in enumerate(self.buttons):
+            current_state = button.value()
+            if current_state != self.button_states[i]:
+                self.button_states[i] = current_state
+                if current_state == 0:  # Button pressed
+                    self.toggle_output(i)
+
+    def toggle_output(self, index):
+        output = self.outputs[index]
+        output.value(not output.value())

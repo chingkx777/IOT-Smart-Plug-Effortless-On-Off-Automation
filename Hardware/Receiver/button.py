ef6b1ad26933b9f645c2b67eebe00d88a9631b2a
chingkx777 <93992946+chingkx777@users.noreply.github.com>
2023-12-08 18:00:05 +0800

    Update button.py

--- Hardware/Receiver/button.py
+++ Hardware/Receiver/button.py
@@ -3,10 +3,11 @@ import time
 import _thread
 
 class button:
-    def __init__(self, button_pins, led_pins):
+    def __init__(self, button_pins, led_pins, special_pin):
         # Define pin and button lists
         self.led_pins = [machine.Pin(pin_num, machine.Pin.OUT) for pin_num in led_pins]
         self.button_pins = [machine.Pin(button_num, machine.Pin.IN, machine.Pin.PULL_UP) for button_num in button_pins]
+        self.special_pin = special_pin
 
         # Initialize variables to track button state
         self.button_states = [True] * len(self.button_pins)
@@ -24,6 +25,20 @@ class button:
         return self.button_states[button_index]
 
     def button_thread(self):
+        def all_pins_on():
+            for led in self.led_pins:
+                led.on()
+                
+        def all_pins_off():
+            for led in self.led_pins:
+                led.off()
+        
+        def get_pin_status():
+            status_list = []
+            for led in self.led_pins:
+                status_list.append(int(led.value()))  # Append 0 for off and 1 for on
+            return status_list
+        
         self.running = True
         while self.running:
             for i in range(len(self.button_pins)):
@@ -32,7 +47,19 @@ class button:
                     led = self.led_pins[i]
                     led.value(not led.value())  # Toggle the corresponding LED pin
                 self.last_button_states[i] = self.button_states[i]
-            time.sleep_ms(10)  # Small delay to avoid busy-waiting
+            pin_value = machine.Pin(self.special_pin, machine.Pin.IN).value()
+            status = get_pin_status()
+            if pin_value == 1 and status == [0, 0, 0]:
+                all_pins_on()
+                time.sleep(1)
+                continue
+            elif pin_value == 1:
+                all_pins_off()
+                time.sleep(1)
+                continue
+            else:
+                time.sleep_ms(10)  # Small delay to avoid busy-waiting
+                continue
 
     def start(self):
         _thread.start_new_thread(self.button_thread, ())
